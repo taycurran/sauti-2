296b7d41e5bd38dd5d5b61c652c02308e954dffc
taycurran <taylorcurranagtc@gmail.com>
2020-06-01 13:43:16 -0500

    Adding DB Route

---
+++ app/data/connection.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+# Create Connection Engine
+# represents the core interface to the database
+engine = create_engine(os.getenv("aws_db_url"), echo=False)
+
+# Define Class ##
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
+
+# returns a class
+Base = declarative_base()
+
+Base.query = db_session.query_property()
+
+def init_db():
+    import app.data.models
+    Base.metadata.create_all(bind=engine)
--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import jsonify 
 import pandas as pd
+from data.connection import db_session
+from data.models import QC_Wholesale
   
 app = Flask(__name__) 
   
@@ -13,3 +15,12 @@ def dummy_qcwholesale():
         df = pd.read_csv("app/qc_wholesale.csv")
         return df.to_json()
 
+@app.route("/qcwholesale/")
+def get_table():
+        return QC_Wholesale.query.all()
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+        db_session.remove()
+
+ 
