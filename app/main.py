87d5d66d58f35b3a9825a72d43644747b22a6855
taycurran <taylorcurranagtc@gmail.com>
2020-06-01 11:54:18 -0500

    Adding CSV Dummy Route

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
-from flask import Flask 
+from flask import Flask
+from flask import jsonify 
+import pandas as pd
   
 app = Flask(__name__) 
   
 @app.route("/") 
 def home_view(): 
         return "<h1>Welcome to Sauti DS</h1>"
+
+@app.route('/dummy_qcwholesale')
+def dummy_qcwholesale():
+        df = pd.read_csv("qc_wholesale.csv")
+        return jsonify(df)
+
