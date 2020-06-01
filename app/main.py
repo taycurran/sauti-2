dcec8aa0c47046656397777d8fddefde198682d8
taycurran <taylorcurranagtc@gmail.com>
2020-06-01 11:30:20 -0500

    Changed to FastAPI

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
-from flask import Flask 
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI(__name__)
   
-app = Flask(__name__) 
-  
-@app.route("/") 
-def home_view(): 
-        return "<h1>Welcome to Sauti DS App</h1>"
+@app.get("/")
+async def root():
+  """
+  Sauti Market Monitor API  
+  Verifies the API is deployed, and links to the docs.
+  """
+  return HTMLResponse("""
+  <h1>Sauti Market Monitor</h1>
+  <p>Go to <a href="/docs">/docs</a> for documentation.</p>
+  """)
