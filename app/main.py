3740c8548f28863725adb3c616e5e7f88a7e3ce4
taycurran <taylorcurranagtc@gmail.com>
2020-06-02 09:58:54 -0500

    Small Change

--- app/main.py
+++ app/main.py
@@ -6,22 +6,22 @@ import pandas as pd
 import psycopg2
 import os
 from dotenv import load_dotenv
-import pandas as pd
-load_dotenv()
+# import pandas as pd
+# load_dotenv()
 
-labs_conn = psycopg2.connect("")
-labs_curs = labs_conn.cursor()
+# labs_conn = psycopg2.connect("")
+# labs_curs = labs_conn.cursor()
 
-Q_select_all = """SELECT * FROM qc_wholesale LIMIT 200;"""
-labs_curs.execute(Q_select_all)
-print("\nSELECT Query Excecuted")
+# Q_select_all = """SELECT * FROM qc_wholesale LIMIT 200;"""
+# labs_curs.execute(Q_select_all)
+# print("\nSELECT Query Excecuted")
 
-DF_QC_WHOLESALE = pd.DataFrame(labs_curs.fetchmany(200))
-print(DF_QC_WHOLESALE.head())
+# DF_QC_WHOLESALE = pd.DataFrame(labs_curs.fetchmany(200))
+# print(DF_QC_WHOLESALE.head())
 
-labs_curs.close()
-labs_conn.close()
-print("Cursor and Connection Closed.")
+# labs_curs.close()
+# labs_conn.close()
+# print("Cursor and Connection Closed.")
   
 app = Flask(__name__) 
   
@@ -36,6 +36,19 @@ def dummy_qcwholesale():
 
 @app.route("/qcwholesale/")
 def get_table():
+        labs_conn = psycopg2.connect("")
+        labs_curs = labs_conn.cursor()
+
+        Q_select_all = """SELECT * FROM qc_wholesale LIMIT 200;"""
+        labs_curs.execute(Q_select_all)
+        print("\nSELECT Query Excecuted")
+
+        DF_QC_WHOLESALE = pd.DataFrame(labs_curs.fetchmany(200))
+        print(DF_QC_WHOLESALE.head())
+
+        labs_curs.close()
+        labs_conn.close()
+        print("Cursor and Connection Closed.")
         result = {}
         for index, row in DF_QC_WHOLESALE.iterrows():
                 result[index] = dict(row)
