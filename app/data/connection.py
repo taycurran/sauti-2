dcee711c7a40c9629d13239fcc760e6b1ad68d3e
taycurran <taylorcurranagtc@gmail.com>
2020-06-02 09:22:47 -0500

    changed it so its all on one page

--- app/data/connection.py
+++ app/data/connection.py
@@ -1,19 +1,19 @@
-import psycopg2
-import os
-from dotenv import load_dotenv
-import pandas as pd
-load_dotenv()
+# import psycopg2
+# import os
+# from dotenv import load_dotenv
+# import pandas as pd
+# load_dotenv()
 
-labs_conn = psycopg2.connect(os.getenv(''))
-labs_curs = labs_conn.cursor()
+# labs_conn = psycopg2.connect(os.getenv('DB_URL'))
+# labs_curs = labs_conn.cursor()
 
-Q_select_all = """SELECT * FROM qc_wholesale LIMIT 200;"""
-labs_curs.execute(Q_select_all)
-print("\nSELECT Query Excecuted")
+# Q_select_all = """SELECT * FROM qc_wholesale LIMIT 200;"""
+# labs_curs.execute(Q_select_all)
+# print("\nSELECT Query Excecuted")
 
-DF_QC_WHOLESALE = pd.DataFrame(labs_curs.fetchmany(200))
-print(DF_QC_WHOLESALE.head())
+# DF_QC_WHOLESALE = pd.DataFrame(labs_curs.fetchmany(200))
+# print(DF_QC_WHOLESALE.head())
 
-labs_curs.close()
-labs_conn.close()
-print("Cursor and Connection Closed.")
+# labs_curs.close()
+# labs_conn.close()
+# print("Cursor and Connection Closed.")
--- app/main.py
+++ app/main.py
@@ -3,7 +3,25 @@ from flask import jsonify
 import pandas as pd
 # from app.data.connection import db_session
 # from app.data.models import QC_Wholesale
-from app.data.connection import DF_QC_WHOLESALE
+import psycopg2
+import os
+from dotenv import load_dotenv
+import pandas as pd
+load_dotenv()
+
+labs_conn = psycopg2.connect(os.getenv('DB_URL'))
+labs_curs = labs_conn.cursor()
+
+Q_select_all = """SELECT * FROM qc_wholesale LIMIT 200;"""
+labs_curs.execute(Q_select_all)
+print("\nSELECT Query Excecuted")
+
+DF_QC_WHOLESALE = pd.DataFrame(labs_curs.fetchmany(200))
+print(DF_QC_WHOLESALE.head())
+
+labs_curs.close()
+labs_conn.close()
+print("Cursor and Connection Closed.")
   
 app = Flask(__name__) 
   
