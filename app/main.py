bd90109b408554169faf6bbe6642a6fad7459d9e
taycurran <taylorcurranagtc@gmail.com>
2020-06-02 15:37:14 -0500

    Changed Data Format

--- app/main.py
+++ app/main.py
@@ -60,6 +60,88 @@ def get_table():
         for index, row in df.iterrows():
                         result.append(dict(row))
         return jsonify(result)
+
+@app.route("/data-quality-rt/")
+def get_table():
+        labs_conn = psycopg2.connect(os.getenv("DATABASE_URL"))
+        labs_curs = labs_conn.cursor()
+
+        Q_select_all = """SELECT * FROM qc_retail;"""
+        labs_curs.execute(Q_select_all)
+        print("\nSELECT * Query Excecuted.")
+
+        rows = labs_curs.fetchall()
+
+        df = pd.DataFrame(rows, columns= [
+                "qc_id", "market", "product", "source",
+                "start", "end", "timeliness", "data_length",
+                "completeness", "duplicates", "mode_D", "data_points",
+                "DQI", "DQI_cat"
+        ])
+
+        labs_curs.close()
+        labs_conn.close()
+        print("Cursor and Connection Closed.")
+        result = []
+        for index, row in df.iterrows():
+                        result.append(dict(row))
+        return jsonify(result)
+
+@app.route("/price-status-ws/")
+def get_table():
+        labs_conn = psycopg2.connect(os.getenv("DATABASE_URL"))
+        labs_curs = labs_conn.cursor()
+
+        Q_select_all = """SELECT product_name, market_id,
+                        source_id, currency_code, observed_price,
+                        observed_class
+                         FROM product_clean_wholesale_info;"""
+        labs_curs.execute(Q_select_all)
+        print("\nSELECT * Query Excecuted.")
+
+        rows = labs_curs.fetchall()
+
+        df = pd.DataFrame(rows, columns= [
+                "product_name", "market_id", "source_id",
+                "currency_code", "date_price", "observed_price",
+                "observed_class"
+        ])
+
+        labs_curs.close()
+        labs_conn.close()
+        print("Cursor and Connection Closed.")
+        result = []
+        for index, row in df.iterrows():
+                        result.append(dict(row))
+        return jsonify(result)
+
+@app.route("/price-status-ws/")
+def get_table():
+        labs_conn = psycopg2.connect(os.getenv("DATABASE_URL"))
+        labs_curs = labs_conn.cursor()
+
+        Q_select_all = """SELECT product_name, market_id,
+                        source_id, currency_code, observed_price,
+                        observed_class
+                         FROM product_clean_retail_info;"""
+        labs_curs.execute(Q_select_all)
+        print("\nSELECT * Query Excecuted.")
+
+        rows = labs_curs.fetchall()
+
+        df = pd.DataFrame(rows, columns= [
+                "product_name", "market_id", "source_id",
+                "currency_code", "date_price", "observed_price",
+                "observed_class"
+        ])
+
+        labs_curs.close()
+        labs_conn.close()
+        print("Cursor and Connection Closed.")
+        result = []
+        for index, row in df.iterrows():
+                        result.append(dict(row))
+        return jsonify(result)
         
 
 
