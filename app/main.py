4e5d67b4a81471b1fbec204521c1a0bca1d25344
taycurran <taylorcurranagtc@gmail.com>
2020-06-02 15:39:47 -0500

    Changed Data Format

--- app/main.py
+++ app/main.py
@@ -87,61 +87,61 @@ def get_table():
                         result.append(dict(row))
         return jsonify(result)
 
-@app.route("/price-status-ws/")
-def get_table():
-        labs_conn = psycopg2.connect(os.getenv("DATABASE_URL"))
-        labs_curs = labs_conn.cursor()
+# @app.route("/price-status-ws/")
+# def get_table():
+#         labs_conn = psycopg2.connect(os.getenv("DATABASE_URL"))
+#         labs_curs = labs_conn.cursor()
 
-        Q_select_all = """SELECT product_name, market_id,
-                        source_id, currency_code, observed_price,
-                        observed_class
-                         FROM product_clean_wholesale_info;"""
-        labs_curs.execute(Q_select_all)
-        print("\nSELECT * Query Excecuted.")
+#         Q_select_all = """SELECT product_name, market_id,
+#                         source_id, currency_code, observed_price,
+#                         observed_class
+#                          FROM product_clean_wholesale_info;"""
+#         labs_curs.execute(Q_select_all)
+#         print("\nSELECT * Query Excecuted.")
 
-        rows = labs_curs.fetchall()
+#         rows = labs_curs.fetchall()
 
-        df = pd.DataFrame(rows, columns= [
-                "product_name", "market_id", "source_id",
-                "currency_code", "date_price", "observed_price",
-                "observed_class"
-        ])
+#         df = pd.DataFrame(rows, columns= [
+#                 "product_name", "market_id", "source_id",
+#                 "currency_code", "date_price", "observed_price",
+#                 "observed_class"
+#         ])
 
-        labs_curs.close()
-        labs_conn.close()
-        print("Cursor and Connection Closed.")
-        result = []
-        for index, row in df.iterrows():
-                        result.append(dict(row))
-        return jsonify(result)
+#         labs_curs.close()
+#         labs_conn.close()
+#         print("Cursor and Connection Closed.")
+#         result = []
+#         for index, row in df.iterrows():
+#                         result.append(dict(row))
+#         return jsonify(result)
 
-@app.route("/price-status-ws/")
-def get_table():
-        labs_conn = psycopg2.connect(os.getenv("DATABASE_URL"))
-        labs_curs = labs_conn.cursor()
+# @app.route("/price-status-ws/")
+# def get_table():
+#         labs_conn = psycopg2.connect(os.getenv("DATABASE_URL"))
+#         labs_curs = labs_conn.cursor()
 
-        Q_select_all = """SELECT product_name, market_id,
-                        source_id, currency_code, observed_price,
-                        observed_class
-                         FROM product_clean_retail_info;"""
-        labs_curs.execute(Q_select_all)
-        print("\nSELECT * Query Excecuted.")
+#         Q_select_all = """SELECT product_name, market_id,
+#                         source_id, currency_code, observed_price,
+#                         observed_class
+#                          FROM product_clean_retail_info;"""
+#         labs_curs.execute(Q_select_all)
+#         print("\nSELECT * Query Excecuted.")
 
-        rows = labs_curs.fetchall()
+#         rows = labs_curs.fetchall()
 
-        df = pd.DataFrame(rows, columns= [
-                "product_name", "market_id", "source_id",
-                "currency_code", "date_price", "observed_price",
-                "observed_class"
-        ])
+#         df = pd.DataFrame(rows, columns= [
+#                 "product_name", "market_id", "source_id",
+#                 "currency_code", "date_price", "observed_price",
+#                 "observed_class"
+#         ])
 
-        labs_curs.close()
-        labs_conn.close()
-        print("Cursor and Connection Closed.")
-        result = []
-        for index, row in df.iterrows():
-                        result.append(dict(row))
-        return jsonify(result)
+#         labs_curs.close()
+#         labs_conn.close()
+#         print("Cursor and Connection Closed.")
+#         result = []
+#         for index, row in df.iterrows():
+#                         result.append(dict(row))
+#         return jsonify(result)
         
 
 
